a3304dfdf0276a63a7a0ebb23a1b433fce1b54fc
Marco De Freitas <marcoantoniodfp@gmail.com>
2023-05-06 22:36:57 -0400

    Creacion main - puliendo validaciones.

---
+++ src/clases/carrito.py
@@ -0,0 +1,11 @@
+class carrito: 
+    # Añadir lista enlazada de productos
+
+    def agregar_producto(self):
+        print("")
+    
+    def eliminar_productos(self):
+        print("")
+
+    def total(self):
+        print("")
